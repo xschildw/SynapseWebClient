4546aa6d1672fa610fe9edb03ba0fb59df49aaed
deflaux <nicole.deflaux@sagebase.org>
2011-12-13 02:04:53 +0000

    Upgraded metadata provider tests.

--- sts/services/repository/src/test/java/org/sagebionetworks/repo/web/controller/metadata/LocationMetadataProviderTest.java
+++ sts/services/repository/src/test/java/org/sagebionetworks/repo/web/controller/metadata/LocationMetadataProviderTest.java
@@ -18,6 +18,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+@Deprecated
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = { "classpath:test-context.xml" })
 public class LocationMetadataProviderTest {
