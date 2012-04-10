cbd35ea51bc16e15825d5cf4391dd30958f1a0d7
deflaux <nicole.deflaux@sagebase.org>
2012-04-10 17:10:37 +0000

    Merging temporary patch for PLFM-1119

--- services/repository/src/test/java/org/sagebionetworks/repo/web/controller/SearchControllerAutowiredTest.java
+++ services/repository/src/test/java/org/sagebionetworks/repo/web/controller/SearchControllerAutowiredTest.java
@@ -10,6 +10,7 @@ import java.util.Map;
 
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.sagebionetworks.repo.manager.TestUserDAO;
@@ -52,6 +53,7 @@ public class SearchControllerAutowiredTest {
 	/**
 	 * @throws Exception
 	 */
+	@Ignore // TODO PLFM-1119
 	@Test
 	public void testSearch() throws Exception {
 		testSearchHelper("q=prostate&return-fields=name", 1);
@@ -60,6 +62,33 @@ public class SearchControllerAutowiredTest {
 	/**
 	 * @throws Exception
 	 */
+	@Ignore // TODO PLFM-1119
+	@Test
+	public void testAllReturnFields() throws Exception {
+		SearchResults result = testSearchHelper(
+				"q=syn4494&return-fields=id,name,description,etag,modified_on,created_on,num_samples,created_by_r,modified_by_r,node_type_r,disease_r,tissue_r",
+				1);
+		if (null != result) {
+			Hit hit = result.getHits().get(0);
+			assertNotNull(hit.getId());
+			assertNotNull(hit.getName());
+			assertNotNull(hit.getDescription());
+			assertNotNull(hit.getEtag());
+			assertNotNull(hit.getModified_on());
+			assertNotNull(hit.getCreated_on());
+			assertNotNull(hit.getNum_samples());
+			assertNotNull(hit.getCreated_by());
+			assertNotNull(hit.getModified_by());
+			assertNotNull(hit.getNode_type());
+			assertNotNull(hit.getDisease());
+			assertNotNull(hit.getTissue());
+		}
+	}
+
+	/**
+	 * @throws Exception
+	 */
+	@Ignore // TODO PLFM-1119
 	@Test
 	public void testFacetedSearch() throws Exception {
 		SearchResults result = testSearchHelper("q=cancer&return-fields=id,name,description,etag&facet=node_type,disease,species,tissue,platform,num_samples,created_by,modified_by,created_on,modified_on,acl,reference", 1);
@@ -73,6 +102,7 @@ public class SearchControllerAutowiredTest {
 	/**
 	 * @throws Exception
 	 */
+	@Ignore // TODO PLFM-1119
 	@Test
 	public void testNoResultsFacetedSearch() throws Exception {
 		SearchResults result = testSearchHelper("q=cancer&bq=node_type:'DoesNotExist'&facet=node_type,species,disease,modified_on,tissue,num_samples,created_by&return-fields=name,description,id", 0);
@@ -86,6 +116,7 @@ public class SearchControllerAutowiredTest {
 	/**
 	 * @throws Exception
 	 */
+	@Ignore // TODO PLFM-1119
 	@Test
 	public void testMultiWordFreeTextSearch() throws Exception {
 		testSearchHelper("q=prostate+cancer&return-fields=name&bq=node_type:'dataset'", 1);
@@ -95,6 +126,7 @@ public class SearchControllerAutowiredTest {
 	/**
 	 * @throws Exception
 	 */
+	@Ignore // TODO PLFM-1119
 	@Test
 	public void testBooleanQuerySearch() throws Exception {
 		testSearchHelper("q=prostate&return-fields=name&bq=node_type:'dataset'", 1);
@@ -103,6 +135,7 @@ public class SearchControllerAutowiredTest {
 	/**
 	 * @throws Exception
 	 */
+	@Ignore // TODO PLFM-1119
 	@Test
 	public void testSearchAuthorizationFilter() throws Exception {
 		SearchResults result = testSearchHelper("q=prostate&return-fields=name", 1);
@@ -119,6 +152,7 @@ public class SearchControllerAutowiredTest {
 	/**
 	 * @throws Exception
 	 */
+	@Ignore // TODO PLFM-1119
 	@Test
 	public void testAnonymousSearchAuthorizationFilter() throws Exception {
 		testHelper.setTestUser(AuthorizationConstants.ANONYMOUS_USER_ID);
@@ -134,6 +168,7 @@ public class SearchControllerAutowiredTest {
 	/**
 	 * @throws Exception
 	 */
+	@Ignore // TODO PLFM-1119
 	@Test
 	public void testAdminSearchAuthorizationFilter() throws Exception {
 		testHelper.setTestUser(TestUserDAO.ADMIN_USER_NAME);
@@ -148,6 +183,7 @@ public class SearchControllerAutowiredTest {
 	/**
 	 * @throws Exception
 	 */
+	@Ignore // TODO PLFM-1119
 	@Test
 	public void testDescription() throws Exception {
 		testHelper.setTestUser(TestUserDAO.ADMIN_USER_NAME);
